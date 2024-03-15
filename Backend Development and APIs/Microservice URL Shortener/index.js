8f4b923ce51cc04ab5dda84598aa24f5c1a76afa
GoonerMAK <mahfuzanan20@gmail.com>
2024-03-16 03:13:27 +0600

    Fixed Test Case 4 issue

--- Backend Development and APIs/Microservice URL Shortener/index.js
+++ Backend Development and APIs/Microservice URL Shortener/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const { urlencoded } = require('body-parser');
 const app = express();
 const dns = require('dns');
+const { url } = require('inspector');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -22,19 +23,20 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+
 const urlDatabase = {};
 let nextShortUrlId = 1;
 
 const isValidUrl = (url) => {
   try {
-    new URL(url);
-    return true;
+    const pattern = /^(https?:\/\/)?([a-zA-Z0-9-]+\.)*([a-zA-Z0-9-]+\.[a-zA-Z]{2,})(\/\S*)?$/;
+    return pattern.test(url);
   } catch (err) {
     return false;
   }
-}
+};
 
-app.post('/api/shorturl', function (req, res) {
+app.post('/api/shorturl', (req, res) => {
   const originalUrl = req.body.url;
 
   if (!isValidUrl(originalUrl)) {
@@ -59,7 +61,7 @@ app.post('/api/shorturl', function (req, res) {
   });
 });
 
-app.get('/api/shorturl/:short_url', function (req, res) {
+app.get('/api/shorturl/:short_url', (req, res) => {
   const shortUrl = req.params.short_url;
   const originalUrl = urlDatabase[shortUrl];
 
