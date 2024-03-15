4170fbffbb32b3bb15eaff79630b37fd2b6c87fa
GoonerMAK <mahfuzanan20@gmail.com>
2024-03-16 01:28:04 +0600

    Microservice URL Shortener Implemented

--- Backend Development and APIs/Microservice URL Shortener/index.js
+++ Backend Development and APIs/Microservice URL Shortener/index.js
@@ -1,24 +1,75 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { urlencoded } = require('body-parser');
 const app = express();
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+const urlDatabase = {};
+let nextShortUrlId = 1;
+
+const isValidUrl = (url) => {
+  try {
+    new URL(url);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+app.post('/api/shorturl', function (req, res) {
+  const originalUrl = req.body.url;
+
+  if (!isValidUrl(originalUrl)) {
+    res.json({ error: 'invalid url' });
+    return;
+  }
+
+  const { hostname } = new URL(originalUrl);
+  dns.lookup(hostname, (err) => {
+    if (err) {
+      res.json({ error: 'invalid hostname' });
+      return;
+    }
+
+    const shortUrl = nextShortUrlId++;
+    urlDatabase[shortUrl] = originalUrl;
+
+    res.json({
+      original_url: originalUrl,
+      short_url: shortUrl
+    });
+  });
+});
+
+app.get('/api/shorturl/:short_url', function (req, res) {
+  const shortUrl = req.params.short_url;
+  const originalUrl = urlDatabase[shortUrl];
+
+  if (originalUrl) {
+    res.redirect(originalUrl);
+  } else {
+    res.json({ error: 'short url not found' });
+  }
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
